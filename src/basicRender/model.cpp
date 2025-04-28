dd1b320b67e7afa50a36d4132e40b2a1a4a30f49
Si-Xiaoming <zbssm123@163.com>
2025-04-29 02:06:14 +0800

    ✨ feat: add shader

--- src/basicRender/model.cpp
+++ src/basicRender/model.cpp
@@ -51,3 +51,111 @@ std::vector<int> Model::face(int idx) {
 Vec3f Model::vert(int i) {
     return verts_[i];
 }
+
+ModelPro::ModelPro(const char* filename) :
+    verts_(), faces_(), norms_(), uv_(), diffusemap_(),
+    normalmap_(), specularmap_() {
+    std::ifstream in;
+    in.open(filename, std::ifstream::in);
+    if (in.fail()) return;
+    std::string line;
+    while (!in.eof()) {
+        std::getline(in, line);
+        std::istringstream iss(line.c_str());
+        char trash;
+        if (!line.compare(0, 2, "v ")) {
+            iss >> trash;
+            Vec3f v;
+            for (int i = 0; i < 3; i++) iss >> v[i];
+            verts_.push_back(v);
+        }
+        else if (!line.compare(0, 3, "vn ")) {
+            iss >> trash >> trash;
+            Vec3f n;
+            for (int i = 0; i < 3; i++) iss >> n[i];
+            norms_.push_back(n);
+        }
+        else if (!line.compare(0, 3, "vt ")) {
+            iss >> trash >> trash;
+            Vec2f uv;
+            for (int i = 0; i < 2; i++) iss >> uv[i];
+            uv_.push_back(uv);
+        }
+        else if (!line.compare(0, 2, "f ")) {
+            std::vector<Vec3i> f;  // f v/vt/vn v/vt/vn v/vt/vn
+            Vec3i tmp;
+            iss >> trash;
+            while (iss >> tmp[0] >> trash >> tmp[1] >> trash >> tmp[2]) {
+                for (int i = 0; i < 3; i++) tmp[i]--; // in wavefront obj all indices start at 1, not zero
+                f.push_back(tmp);
+            }
+            faces_.push_back(f);
+        }
+    }
+    std::cerr << "# v# " << verts_.size() << " f# " << faces_.size() << " vt# " << uv_.size() << " vn# " << norms_.size() << std::endl;
+    load_texture(filename, "_diffuse.tga", diffusemap_);
+    load_texture(filename, "_nm.tga", normalmap_);
+    load_texture(filename, "_spec.tga", specularmap_);
+}
+ModelPro::~ModelPro() {}
+int ModelPro::nverts() {
+    return (int)verts_.size();
+}
+
+int ModelPro::nfaces() {
+    return (int)faces_.size();
+}
+std::vector<int> ModelPro::face(int idx) {
+    std::vector<int> face;
+    for (int i = 0; i < (int)faces_[idx].size(); i++) face.push_back(faces_[idx][i][0]);  //f v/vt/vn
+    return face;
+}
+Vec3f ModelPro::vert(int i) {
+    return verts_[i];
+}
+
+Vec3f ModelPro::vert(int iface, int nthvert) {
+    return verts_[faces_[iface][nthvert][0]];
+}
+void ModelPro::load_texture(std::string filename, const char* suffix, TGAImage& img, std::string fold) {
+    std::string texfile(filename);
+    
+    size_t dot = texfile.find_last_of(".");
+	size_t lastSlash = texfile.find_last_of("/\\");
+    // extract the substr between lastSlash and dot
+	std::string texname = texfile.substr(lastSlash + 1, dot - lastSlash - 1);
+    texfile = fold + texname + std::string(suffix);
+
+    std::cerr << "texture file " << texfile << " loading " << (img.read_tga_file(texfile.c_str()) ? "ok" : "failed") << std::endl;
+    img.flip_vertically();
+
+
+
+
+}
+TGAColor ModelPro::diffuse(Vec2f uvf) {
+    Vec2i uv(uvf[0] * diffusemap_.get_width(), uvf[1] * diffusemap_.get_height());
+    return diffusemap_.get(uv[0], uv[1]);
+}
+Vec3f ModelPro::normal(Vec2f uvf) {
+    Vec2i uv(uvf[0] * normalmap_.get_width(), uvf[1] * normalmap_.get_height());
+    TGAColor c = normalmap_.get(uv[0], uv[1]);
+    Vec3f res;
+    for (int i = 0; i < 3; i++)
+        res[2 - i] = (float)c[i] / 255.f * 2.f - 1.f;
+    return res;
+}
+
+Vec2f ModelPro::uv(int iface, int nthvert) {
+    return uv_[faces_[iface][nthvert][1]];
+}
+
+float ModelPro::specular(Vec2f uvf) {
+    Vec2i uv(uvf[0] * specularmap_.get_width(), uvf[1] * specularmap_.get_height());
+    return specularmap_.get(uv[0], uv[1])[0] / 1.f;
+}
+
+Vec3f ModelPro::normal(int iface, int nthvert) {
+    int idx = faces_[iface][nthvert][2];
+    return norms_[idx].normalize();
+}
--- src/basicRender/model.h
+++ src/basicRender/model.h
@@ -3,6 +3,7 @@
 
 #include <vector>
 #include "geometry.h"
+#include<tgaimage/tgaimage.h>
 
 class Model {
 private:
@@ -14,6 +15,34 @@ public:
 	int nverts();
 	int nfaces();
 	Vec3f vert(int i);
+	std::vector<int> face(int idx);
+};
+
+class ModelPro {
+private:
+	std::vector<Vec3f> verts_;
+	std::vector<std::vector<Vec3i> > faces_;
+
+	std::vector<Vec3f> norms_;
+	std::vector<Vec2f> uv_;
+	TGAImage diffusemap_;
+	TGAImage normalmap_;
+	TGAImage specularmap_;
+
+	void load_texture(std::string filename, const char* suffix, TGAImage& img, std::string fold="data/images/");
+public:
+	ModelPro(const char* filename);
+	~ModelPro();
+	int nverts();
+	int nfaces();
+	Vec3f normal(int iface, int nthvert);
+	Vec3f normal(Vec2f uv);
+	Vec3f vert(int i);
+	Vec3f vert(int iface, int nthvert);
+	Vec2f uv(int iface, int nthvert);
+
+	TGAColor diffuse(Vec2f uv);
+	float specular(Vec2f uv);
 	std::vector<int> face(int idx);
 };
 
